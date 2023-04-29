08411899ff17313deef08aae779dee4a5d4e3d15
kylesteinmann <105984447+kylesteinmann@users.noreply.github.com>
2023-04-28 21:10:56 -0500

    Changes Co-authored-by: Heath Falkenrath <theGeekkit@users.noreply.github.com> Co-authored-by: chrisHembree <chrisHembree@users.noreply.github.com>

--- src/app/items/item.ts
+++ src/app/items/item.ts
@@ -1,9 +1,11 @@
 export class Item {
+  id="";
   name = "";
   description = "";
   price = "";
   stock = "";
-  constructor(name="", description="", price="", stock="") {
+  constructor(name="", description="", price="", stock="", id="") {
+    this.id = id;
     this.name = name;
     this.description = description;
     this.price = price;
--- src/app/items/items.service.ts
+++ src/app/items/items.service.ts
@@ -12,12 +12,34 @@ export class ItemsService {
   price = "";
   stock = "";
 
+  items: Item[] = [];
+  cart: Item[] = [];
 
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.getItems()
+   }
 
 
   onSubmitItem(Form: NgForm) {
-    return this.http.post<Item>('http://localhost:3000/inventories', Form.value).subscribe()
+    return this.http.post<Item>('http://localhost:3000/inventories', Form.value).subscribe(() =>
+      this.getItems()
+
+
+    )
+
+  }
+
+  getItems() {
+    return this.http.get<Item[]>('http://localhost:3000/inventories').subscribe(data => this.items=data)
+
+  }
+
+  addToCart(item: Item) {
+    this.cart.push(item);
+  }
+
+  onCheckout() {
+    return this.http.post<Item[]>('http://localhost:3000/carts', this.cart).subscribe()
   }
 }
+
